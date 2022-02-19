3c0e6cc3f86bdb4b4099b459e2ff17d39893fbe7
fvn70 <fvn_reg@mail.ru>
2022-02-19 22:07:05 +0700

    Stage 2/7

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,2 +1,11 @@
-x, y = map(int, input().strip().split(' '))
-print(x + y)
+while True:
+    cmd = input().strip()
+    if cmd == '/exit':
+        break
+    if cmd:
+        sum = 0
+        numbs = map(int, cmd.split(' '))
+        for num in numbs:
+            sum += num
+        print(sum)
+print('Bye!')
