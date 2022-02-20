45251d4b5f863be7cd83eaf6d22c4b004cae2e23
fvn70 <fvn_reg@mail.ru>
2022-02-20 10:34:19 +0700

    Stage 5/7

--- Smart Calculator/task/calculator/calculator.py
+++ Smart Calculator/task/calculator/calculator.py
@@ -1,3 +1,49 @@
+import re
+
+
+def parse(str):
+    str = str.replace('---', '-')
+    str = str.replace('--', '+')
+    str = re.sub('[+]+', '+', str)
+    str = re.sub('\\s+', ' ', str)
+    return str
+
+
+def isdig(num):
+    if num[0] in '+-':
+        return num[1:].isdigit()
+    else:
+        return num.isdigit()
+
+
+def calc(exp):
+    res = 0
+    op = '+'
+    for s in exp.split(' '):
+        if isdig(s):
+            if op == '+':
+                res += int(s)
+            elif op == '-':
+                res -= int(s)
+            else:
+                res = int(s)
+        else:
+            op = s
+    return res
+
+
+def isvalid(exp):
+    parts = exp.split(' ')
+    i = 0
+    for p in parts:
+        if i % 2 == 0 and not isdig(p):
+            return False
+        if i % 2 != 0 and not p in '+-':
+            return False
+        i += 1
+    return True
+
+
 while True:
     cmd = input().strip()
     if cmd == '/exit':
@@ -5,9 +51,12 @@ while True:
     if cmd == '/help':
         print('The program calculates the sum of numbers')
     elif cmd:
-        sum = 0
-        numbs = map(int, cmd.split(' '))
-        for num in numbs:
-            sum += num
-        print(sum)
+        if cmd[0] == '/':
+            print("Unknown command")
+        else:
+            exp = parse(cmd)
+            if isvalid(exp):
+                print(calc(exp))
+            else:
+                print("Invalid expression")
 print('Bye!')
